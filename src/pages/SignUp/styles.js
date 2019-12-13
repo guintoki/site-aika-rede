326437eef08b319e838b3e49d5eac30469bee09e
Guilherme Vilas Boas Roberto <55093125+guintoki@users.noreply.github.com>
2019-12-13 15:52:49 -0200

    alteração bottão

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -39,13 +39,36 @@ width: 400px;
     }
   }
   button {
-    color: black;
-    font-size: 16px;
-    background: #fc6963;
-    height: 56px;
-    border: 0;
-    border-radius: 5px;
-    width: 100%;
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 14px;
+      color: #ffffff;
+      padding: 13px 30px;
+      background: -webkit-gradient(
+        linear, left top, left bottom,
+        from(#05a9f5),
+        color-stop(0.50, #3b3b3b),
+        color-stop(0.50, #242424),
+        to(#59947c));
+      background: linear-gradient(
+        top,
+        #05a9f5 0%,
+        #3b3b3b 50%,
+        #242424 50%,
+        #59947c);
+      border-radius: 17px;
+      border: 1px solid #00acfc;
+      box-shadow:
+        0px 1px 3px rgba(0,200,250,0.5),
+        inset 0px 0px 0px rgba(237,0,237,0);
+      text-shadow:
+        0px -1px 0px rgba(000,000,000,1),
+        0px 1px 0px rgba(255,255,255,0.2);    
+  }
+  button:hover{
+    background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #468ccf), color-stop(1, #63b8ee) );
+	background:-moz-linear-gradient( center top, #468ccf 5%, #63b8ee 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#468ccf', endColorstr='#63b8ee');
+	background-color:#468ccf;
   }
   hr {
     margin: 20px 0;
